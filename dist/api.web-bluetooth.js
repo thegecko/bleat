4a9a6231c3df73da722a2bc58575a1b468349d4b
Rob Moran <rob.moran@arm.com>
2017-06-06 00:17:54 +0100

    Return characteristic when starting or stopping notifications. Resolves #55

--- dist/api.web-bluetooth.js
+++ dist/api.web-bluetooth.js
@@ -499,14 +499,18 @@
             adapter.enableNotify(this._handle, function(dataView) {
                 this.value = dataView;
                 this.dispatchEvent({ type: "characteristicvaluechanged", bubbles: true });
-            }.bind(this), resolve, wrapReject(reject, "startNotifications error"));
+            }.bind(this), function() {
+                resolve(this);
+            }.bind(this), wrapReject(reject, "startNotifications error"));
         }.bind(this));
     };
     BluetoothRemoteGATTCharacteristic.prototype.stopNotifications = function() {
         return new Promise(function(resolve, reject) {
             if (!this.service.device.gatt.connected) return reject("stopNotifications error: device not connected");
 
-            adapter.disableNotify(this._handle, resolve, wrapReject(reject, "stopNotifications error"));
+            adapter.disableNotify(this._handle, function() {
+                resolve(this);
+            }.bind(this), wrapReject(reject, "stopNotifications error"));
         }.bind(this));
     };
     BluetoothRemoteGATTCharacteristic.prototype.addEventListener = createListenerFn([
